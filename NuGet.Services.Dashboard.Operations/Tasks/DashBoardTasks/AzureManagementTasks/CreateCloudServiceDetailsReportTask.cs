b965fa7c68cd9090809feba911388e47d0cec083
zhili1208 <zhili128@outlook.com>
2014-07-21 09:35:00 -0700

    adjust format

--- NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/AzureManagementTasks/CreateCloudServiceDetailsReportTask.cs
+++ NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/AzureManagementTasks/CreateCloudServiceDetailsReportTask.cs
@@ -107,7 +107,7 @@ namespace NuGetGallery.Operations
                 new SendAlertMailTask
                     {
                         AlertSubject = string.Format("Error: Alert for Multiple role instances statuses for {0}", ServiceName),
-                        Details = string.Format("More than half instances of {0} cloud service: {1} is not in ReadyRole status",  ServiceName, unReadyReport),
+                        Details = string.Format("More than half instances of {0} cloud service are not in ReadyRole status, the instance name and status is {2}",  ServiceName, unReadyReport),
                         AlertName = string.Format("Error: Alert for Multiple Role Instances for {0}", ServiceName), //ensure uniqueness in Alert name as that is being used incident key in pagerduty.
                         Component = "CloudService" + ServiceName,
                         Level = "Error"
