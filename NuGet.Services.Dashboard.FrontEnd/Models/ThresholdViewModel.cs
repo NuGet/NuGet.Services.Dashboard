f4caa10bb601d28c4f5a368774ad3357832d9d2f
zhili1208 <zhili128@outlook.com>
2014-07-22 11:05:59 -0700

    add threshold control

---
+++ NuGet.Services.Dashboard.FrontEnd/Models/ThresholdViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NuGetDashboard.Models
+{
+    public class ThresholdViewModel
+    {
+        public string ID { get; set; }
+        public string Type { get; set; }
+
+        public int standard { get; set; }
+
+        public int current { get; set; }
+
+        public ThresholdViewModel(string ID, string Type, int standard, int current)
+        {
+            this.ID = ID;
+            this.Type = Type;
+            this.standard = standard;
+            this.current = current;
+        }
+
+        public ThresholdViewModel() { }
+    }
+}
