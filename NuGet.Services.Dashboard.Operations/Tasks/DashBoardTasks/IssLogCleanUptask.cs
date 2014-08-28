f4ec4bae7109c6fc9abd0e1b0f58b73de12b4a36
zhili1208 <zhili128@outlook.com>
2014-08-28 12:03:35 -0700

    add ISS clean

---
+++ NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/IssLogCleanUptask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NuGetGallery.Operations.Tasks.DashBoardTasks
+{
+    [Command("IssCleanUpTask", "clean up old iss log in current dir", AltName = "clean")]
+    class IssLogCleanUptask : OpsTask
+    {
+        public override void ExecuteCommand()
+        {
+            try
+            {
+                var dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, "*.log");
+                foreach(string dir in dirs)
+                {
+                    string date = dir.Substring(Environment.CurrentDirectory.Length+5, 8);
+                    DateTime logdate = new DateTime();
+                    logdate = DateTime.ParseExact(date, "yyMMddHH", null);
+                    if (logdate < DateTime.UtcNow.AddHours(-48)) Directory.Delete(dir, true);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("clean up error: {0}", e.Message);
+            }
+        }
+    }
+}
