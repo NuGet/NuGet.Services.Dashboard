b54bf49cb9617c995beb4c83561c34f1d3627582
zhili1208 <zhili128@outlook.com>
2014-08-28 13:58:15 -0700

    fix CR

--- NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/IssLogCleanUptask.cs
+++ NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/IssLogCleanUptask.cs
@@ -8,22 +8,29 @@ namespace NuGetGallery.Operations.Tasks.DashBoardTasks
     {
         public override void ExecuteCommand()
         {
-            try
+            var dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, "*.log");
+            foreach (string dir in dirs)
             {
-                var dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, "*.log");
-                foreach(string dir in dirs)
+                try
                 {
-                    string date = dir.Substring(Environment.CurrentDirectory.Length+5, 8);
+                    string date = RemoveLetter(dir.Substring(Environment.CurrentDirectory.Length));
                     DateTime logdate = new DateTime();
                     logdate = DateTime.ParseExact(date, "yyMMddHH", null);
                     if (logdate < DateTime.UtcNow.AddHours(-48)) Directory.Delete(dir, true);
                 }
-
+                catch (Exception e)
+                {
+                    Console.WriteLine("clean up error: {0}", e.Message);
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("clean up error: {0}", e.Message);
-            }
+        }
+        private string RemoveLetter(string src)
+        {
+            if (src.Equals("")) return src;
+            if (char.IsDigit(src[0])) return src[0] + RemoveLetter(src.Substring(1));
+            else return RemoveLetter(src.Substring(1));
         }
     }
 }
+ 
+
