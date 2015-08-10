7e54426cf2f253356e6ec522e704a338312ec092
Maarten Balliauw <maarten@maartenballiauw.be>
2015-08-10 13:08:32 +0200

    Status page messages now send by dashboard instead of user

--- NuGet.Services.Dashboard.FrontEnd/Controllers/StatusPageController.cs
+++ NuGet.Services.Dashboard.FrontEnd/Controllers/StatusPageController.cs
@@ -22,8 +22,7 @@ namespace NuGetDashboard.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (TableStorageService.WriteStatusPageMessage(model.Environment, model.When, model.Prefix + model.Message, 
-                    ((ClaimsIdentity)User.Identity).FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value))
+                if (TableStorageService.WriteStatusPageMessage(model.Environment, model.When, model.Prefix + model.Message, "dashboard"))
                 {
                     return RedirectToAction("Create", new { posted = true });
                 }
