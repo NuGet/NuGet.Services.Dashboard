972d7a43efb5de38bc770460906b30ee8bd3e820
Maarten Balliauw <maarten@maartenballiauw.be>
2016-09-26 08:54:11 +0200

    Remove unused variables

--- NuGet.Services.Dashboard.FrontEnd/Filters/EnsureTeamClaimsAttribute.cs
+++ NuGet.Services.Dashboard.FrontEnd/Filters/EnsureTeamClaimsAttribute.cs
@@ -14,9 +14,6 @@ namespace NuGetDashboard.Filters
     public sealed class EnsureTeamClaimsAttribute 
         : AuthorizeAttribute
     {
-        private const string IdentityProviderClaimType = "http://schemas.microsoft.com/identity/claims/identityprovider";
-        private const string NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
-
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
             return httpContext.User.Identity.IsAuthenticated
@@ -36,7 +33,7 @@ namespace NuGetDashboard.Filters
 
             filterContext.Result = new ContentResult { Content = @"
                 <html>
-                    <head><title>You didn't say the magic word, " + filterContext.HttpContext.User.Identity.Name + @".</title></head>
+                    <head><title>You didn't say the magic word.</title></head>
 
                     <body><h1>401 - Unauthorized</h1><hr /><p>You did not say the magic word.<br /><img src=""/Images/nedry.gif"" /></p></body>
                 </html>
